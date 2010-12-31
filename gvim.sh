1686f7f9824ede284a07d485dfb4210f06166f16
O01eg <o01eg@yandex.ru>
2010-12-31 11:39:44 +0300

    Remove Ruby support.

--- gvim.sh
+++ gvim.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 find -iname .*.swp | xargs rm
-gvim NEWS README ChangeLog src/*.h src/*.cpp src/libfunctions/*.cpp src/ruby/*.cpp src/ruby/*.h
+gvim NEWS README ChangeLog src/*.h src/*.cpp src/libfunctions/*.cpp
