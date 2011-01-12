3822f2f378dc483d73933107a6c19d320239c948
O01eg <o01eg@yandex.ru>
2011-01-12 14:27:20 +0300

    Change GA parameters.

--- data/config.scm
+++ data/config.scm
@@ -1,11 +1,11 @@
 (begin
 	(define config (make-hash-table))
-	(hashq-set! config 'population-size 32)
-	(hashq-set! config 'max-step-unchanged 20000)
+	(hashq-set! config 'population-size 50)
+	(hashq-set! config 'max-step-unchanged 100000)
 	(hashq-set! config 'max-object-depth 16)
-	(hashq-set! config 'max-individual-adfs 32)
+	(hashq-set! config 'max-individual-adfs 36)
 	(hashq-set! config 'max-stop-moves 4)
-	(hashq-set! config 'max-evalation-loops 10000)
-	(hashq-set! config 'max-individual-steps 16)
+	(hashq-set! config 'max-evalation-loops 20000)
+	(hashq-set! config 'max-individual-steps 12)
 )
 
