a4901a2d10afc619d17cd1fac8eb1d70bba63d72
O01eg <o01eg@yandex.ru>
2011-07-24 23:53:07 +0400

    Fix population size and showed files.

--- data/config.scm
+++ data/config.scm
@@ -1,6 +1,6 @@
 (begin
 	(define config (make-hash-table))
-	(hashq-set! config 'population-size 64)
+	(hashq-set! config 'population-size 75)
 	(hashq-set! config 'max-step-unchanged 10000)
 	(hashq-set! config 'max-object-depth 16)
 	(hashq-set! config 'max-individual-adfs 32)
