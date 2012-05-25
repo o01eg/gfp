8e9748e90aa1690b6d0e014302c51fb204328c18
O01eg <o01eg@yandex.ru>
2012-05-25 23:38:30 +0400

    Prepare to test type system.

--- src/gfpstatic.cpp
+++ src/gfpstatic.cpp
@@ -31,135 +31,32 @@
 #include <vm/object.h>
 #include <vm/ioobject.h>
 #include <vm/logger.h>
-
-struct TypeSystem
-{
-	TypeSystem(VM::Environment& env)
-		:type_int(env.DefineSymbol("TYPE:INT"))
-		,type_bool(env.DefineSymbol("TYPE:BOOL"))
-		,value_true(env.DefineSymbol("TRUE"))
-		,value_false(env.DefineSymbol("FALSE"))
-	{
-		symbols.insert(env.DefineSymbol("TYPE:SYM-DIV-BY-ZERO"));
-		symbols.insert(env.DefineSymbol("TYPE:EMPTY-LIST"));
-	}
-
-	std::set<VM::Object, VM::WeakObject::Comparator> symbols;
-
-	VM::Object type_int;
-	VM::Object type_bool;
-	VM::Object value_true;
-	VM::Object value_false;
-};
-
-/// @todo Get type for expression
-/// @todo Generate expression for type include functional
-
-VM::Object type_to_expr(const TypeSystem& type_system, const VM::Object& type)
-{
-	VM::Object expr(type.GetEnv(), VM::ERROR);
-	switch(type.GetType())
-	{
-		case VM::SYMBOL:
-			{
-				// Generate TYPE:INTEGER
-				if(type == type_system.type_int)
-				{
-					signed long value = static_cast<signed long>(rand()) - RAND_MAX / 2;
-					expr = VM::Object(type.GetEnv(), VM::INTEGER, static_cast<VM::Heap::UInt>(value));
-					return expr;
-				}
-
-				// Generate TYPE:BOOL
-				if(type == type_system.type_bool)
-				{
-					return (rand() % 2) ? type_system.value_true : type_system.value_false;
-				}
-				
-				// Generate TYPE:SYM-*
-				std::set<VM::Object>::const_iterator it = type_system.symbols.find(type);
-				if(it != type_system.symbols.end())
-				{
-					return *it;
-				}
-			}
-			break;
-		case VM::ERROR:
-		case VM::INTEGER:
-		case VM::FUNC:
-		case VM::ADF:
-		case VM::PARAM:
-		case VM::QUOTE:
-		case VM::IF:
-		case VM::LIST:
-		case VM::EVAL:
-		case VM::MACRO:
-		default:
-			std::cout << "Wrong or unsupported type " << type.GetType() << std::endl;
-	}
-	return expr;
-}
-
-VM::Object expr_to_type(const TypeSystem& type_system, const VM::Object& expr)
-{
-	VM::Object type(expr.GetEnv(), VM::ERROR);
-	switch(expr.GetType())
-	{
-		case VM::INTEGER:
-			return type_system.type_int;
-			break;
-		case VM::SYMBOL:
-			{
-				// check for TYPE:BOOL
-				if((expr == type_system.value_true) || (expr == type_system.value_false))
-				{
-					return type_system.type_bool;
-				}
-
-				// check for TYPE:SYM-*
-				std::set<VM::Object>::const_iterator it = type_system.symbols.find(expr);
-				if(it != type_system.symbols.end()) // Get typed symbols
-				{
-					return *it;
-				}
-				/// @todo Other symbols.
-			}
-			break;
-		case VM::ERROR:
-		case VM::FUNC:
-		case VM::ADF:
-		case VM::PARAM:
-		case VM::QUOTE:
-		case VM::IF:
-		case VM::LIST:
-		case VM::EVAL:
-		case VM::MACRO:
-		default:
-			std::cout << "Wrong or unsupported type " << expr.GetType() << std::endl;
-	}
-	return type;
-}
+#include <unordered_map>
 
 int main(int argc, char **argv)
 {
 	srand(time(NULL));
 	{
 		VM::Environment env;
+		// Create 3 types: TYPE-A, TYPE-B, TYPE-C
+		
+		VM::Object type_a = env.DefineSymbol("TYPE-A");
+		VM::Object type_b = env.DefineSymbol("TYPE-B");
+		VM::Object type_c = env.DefineSymbol("TYPE-C");
 
-		TypeSystem type_system(env);
+		// Typed constants and generated expressions.
+		std::unordered_multimap<VM::Object, VM::Object> type_table;
+		type_table.insert(
+			{{type_a, env.DefineSymbol("A1")}
+			,{type_a, env.DefineSymbol("A2")}
+			,{type_b, env.DefineSymbol("B1")}
+			,{type_b, env.DefineSymbol("B2")}
+			,{type_c, env.DefineSymbol("C1")}
+			,{type_c, env.DefineSymbol("C2")}});
 
-		if((argc >= 2) && (argv[1][0] == 't')) // type to expression
-		{
-			VM::Object type(env);
-			std::cin >> type;
-			std::cout << type_to_expr(type_system, type) << std::endl;
-		}
-		else // expression to type
-		{
-			VM::Object expr(env);
-			std::cin >> expr;
-			std::cout << expr_to_type(type_system, expr) << std::endl;
-		}
+		// ( A . B ) mean A -> B
+		// Add A -> B
+		type_table.insert({VM::Object(type_a, type_b), env.DefineSymbol("_AB")});
 	}
 	return 0;
 }
